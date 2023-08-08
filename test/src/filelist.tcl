ccc26c2d296bccbec26cebe9b27f4d4392e58f0a
mingo99 <2839131656@qq.com>
2023-08-08 19:59:32 +0800

    update readme file

--- test/src/filelist.tcl
+++ test/src/filelist.tcl
@@ -8,6 +8,8 @@ set rtl_vhdl_files {
 set tb_verilog_files {
 	tb_clk_div.sv
 	tb_top.sv
+	tb_clocking.sv
+	tb_com.sv
 }
 
 set tb_vhdl_files {
---
+++ test/src/tb/tb_com.sv
@@ -0,0 +1,43 @@
+class Car;
+
+	semaphore key;
+	function new();
+		key = new(0);
+	endfunction
+
+	task stall();
+		$display("car::stall started");
+		#1ns;
+		key.put();
+		$display("stall put");
+		key.get();
+		$display("stall get");
+		$display("car::stall finished");
+	endtask
+
+	task park();
+		key.get();
+		$display("park get");
+		$display("car::park started");
+		#1ns;
+		key.put();
+		$display("park put");
+		$display("car::park finished");
+	endtask
+
+	task drive();
+		fork
+			this.stall();
+			this.park();
+		join_none
+	endtask
+
+endclass
+
+module tb_com;
+	Car car = new();
+	
+	initial begin
+		car.drive();
+	end
+endmodule;
