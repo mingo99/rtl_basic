69510cbe88010611e35874746b1392efe182f1a8
mingo99 <2839131656@qq.com>
2023-11-05 06:51:30 +0800

    disable format for port list

--- fifo/src/filelist.tcl
+++ fifo/src/filelist.tcl
@@ -9,7 +9,7 @@ set rtl_vhdl_files {
 
 set tb_verilog_files {
     tb_sync_fifo.sv
-	tb_async_fifo.sv    
+    tb_async_fifo.sv    
 }
 
 set tb_vhdl_files {
--- fifo/src/rtl/fifo_mem.sv
+++ fifo/src/rtl/fifo_mem.sv
@@ -1,16 +1,14 @@
 module fifo_mem #(
     parameter int DWIDTH = 8,
     parameter int AWIDTH = 4
-) (
-    input  wire              clk,
-    wren,
-    input  wire [AWIDTH-1:0] waddr,
-    raddr,
+) (  // verilog_format: off
+    input  wire              clk, wren,
+    input  wire [AWIDTH-1:0] waddr, raddr,
     input  wire [DWIDTH-1:0] wdata,
     output wire [DWIDTH-1:0] rdata
-);
+); // verilog_format: on
 
-    // RTL Verilog memory model
+   // RTL Verilog memory model
     localparam int DEPTH = 1 << AWIDTH;
     reg [DWIDTH-1:0] mem[DEPTH];
     assign rdata = mem[raddr];
